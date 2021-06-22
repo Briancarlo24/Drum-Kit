6f0ce7d7d1f6f7796f176dbab44fac0990d42816
Brian Carlo Birondo <73984281+Briancarlo24@users.noreply.github.com>
2021-06-22 10:09:35 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+
+//Detecting Button Press
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+  });
+}
+
+//Detecting Keyboard Press
+addEventListener("keypress", function(e){
+  makeSound(e.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default: console.log(buttonInnerHTML);
+  }
+}
+
+
+// document.querySelectorAll("button")[0].addEventListener("click", function (){
+//   var audio = new Audio("sounds/crash.mp3");
+//   audio.play();
+// });
+// document.querySelectorAll("button")[1].addEventListener("click", function (){
+//   var audio = new Audio("sounds/kick-bass.mp3");
+//   audio.play();
+// });
+// document.querySelectorAll("button")[2].addEventListener("click", function () {
+//   var audio = new Audio("sounds/snare.mp3");
+//   audio.play();
+// });
+// document.querySelectorAll("button")[3].addEventListener("click", function () {
+//   var audio = new Audio("sounds/tom-1.mp3");
+//   audio.play();
+// });
+// document.querySelectorAll("button")[4].addEventListener("click", function () {
+//   var audio = new Audio("sounds/tom-2.mp3");
+//   audio.play();
+// });
+// document.querySelectorAll("button")[5].addEventListener("click", function () {
+//   var audio = new Audio("sounds/tom-3.mp3");
+//   audio.play();
+// });
+// document.getElementById("btn7").addEventListener("click", tom4);
+//
+// function tom4() {
+//   var audio = new Audio('sounds/tom-4.mp3');
+//   audio.play();
+// }
+
+//
+// OPTIONAL
+// function handleClick() {
+//   alert("I got Clicked!");
+// }
